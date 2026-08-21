64c67cc7c13823abbc7230b3d5913ccd2dcea5e9
YahiaAbdalla2712 <yayaabdalla2712@gmail.com>
2026-08-21 02:02:10 +0200

    added Tavily search tool

--- webSearchAgent.py
+++ webSearchAgent.py
@@ -6,29 +6,17 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
-
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for 
-    Returns:
-        The search result    
-    """
-    print(f"searching for {query}")
-    return "Tokyo is sunny"
+from langchain_tavily import TavilySearch
 
 
 llm = ChatOllama(temperature=0,model = "qwen2.5:3b")
-tools = [search]
+tools = [TavilySearch(max_results = 3,topic="general",search_depth="advanced")]
 agent = create_agent(model = llm, tools = tools)
 
 def webSearchAgent():
     print("hello from web search agent")
-    result = agent.invoke({"messages": HumanMessage(content = "what is the weather in Tokyo")})
-    print(result["messages"][-1])
+    result = agent.invoke({"messages": HumanMessage(content = "search for 3 job postings for an ai engineer using langchain in the area on linkedin and list their details")})
+    print(result)
 
 
 
