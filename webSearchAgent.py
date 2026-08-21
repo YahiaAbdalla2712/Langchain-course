177e0e07628fef7fc6bc2e3c55f68dee77856974
YahiaAbdalla2712 <yayaabdalla2712@gmail.com>
2026-08-21 04:02:35 +0200

    Added structured output using Pydantic but qwen2.5:3b doesn't support structured output so try to use openAi models

--- webSearchAgent.py
+++ webSearchAgent.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
 from langchain.agents import create_agent
@@ -9,14 +10,26 @@ from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
 
+class Source(BaseModel):
+    """schema for a source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 llm = ChatOllama(temperature=0,model = "qwen2.5:3b")
 tools = [TavilySearch(max_results = 3,topic="general",search_depth="advanced")]
-agent = create_agent(model = llm, tools = tools)
+agent = create_agent(model = llm, tools = tools, response_format=AgentResponse)
 
 def webSearchAgent():
     print("hello from web search agent")
     result = agent.invoke({"messages": HumanMessage(content = "search for 3 job postings for an ai engineer using langchain in the area on linkedin and list their details")})
     print(result)
+    
 
 
 
